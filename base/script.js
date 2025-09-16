328c700cbdedfa2e2283ff29dd7e8431b1b1400f
zahinAlMahmud <167842947+zahinAlMahmud@users.noreply.github.com>
2025-09-16 10:15:55 -0700

    Create script.js

---
+++ base/script.js
@@ -0,0 +1,1 @@
+const hamburger = document.getElementById('hamburger'); const menu = document.getElementById('menu-link');  hamburger.addEventListener('click', () => {     menu.classList.toggle('active'); });
